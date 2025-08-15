c946493a4717048aeaaa16bea7ed2f3fff1cac6f
Emmanuel dakok <dakoke16@gmail.com>
2025-08-15 20:09:28 +0100

    second commit on new-branch.

---
+++ assets/emann.js
@@ -0,0 +1,21 @@
+const menu1 = [
+    {name: "Home", path: "/home", icon: "home"},
+    {name: "Profile", path: "/profile", icon: "user"},
+    {name: "Settings", path: "/settings", icon: "cog"},
+];
+
+const menu2 = [
+    {name: "Home", path: "/home", icon: "home"},
+    {name: "Profile", path: "/profile", icon: "user"},
+    {name: "Settings", path: "/settings", icon: "cog"},
+    {name: "About", path: "/about", icon: "info"},
+    {name: "Contact", path: "/contact", icon: "envelope"},
+];
+
+const menu3 = [
+    {name: "Home", path: "/home", icon: "home"},
+    {name: "Profile", path: "/profile", icon: "user"},
+    {name: "Settings", path: "/settings", icon: "cog"},
+    {name: "About", path: "/about", icon: "info"},
+    {name: "Contact", path: "/contact", icon: "envelope"},
+];
